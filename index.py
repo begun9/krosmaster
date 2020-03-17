ae8ee9dbad19ad7f53544bfe62bf3439bf6998a4
hq015 <k.slava_08@mail.ru>
2020-03-17 16:02:36 +0700

    Test

--- index.py
+++ index.py
@@ -1,17 +1,31 @@
 import socket
 import persone
+import pickle
+
+class Heroes(object):
+    def __init__(self, level, dam, deff, item):
+        self.level = level
+        self.dam = dam
+        self.deff = deff
+        self.item = item
+
+    def levelheroes(self):
+        return [self.level.encode('utf-8'), self.level.encode('utf-8'), self.level.encode('utf-8')]
 
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind(('localhost',5050))
+level = Heroes('a', 'b', 'c', 'd')
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('localhost', 5050))
 client = [] # Массив где храним адреса клиентов
 print('Start Server')
 while 1:
     data, addres = sock.recvfrom(1024)
     print(addres[0], addres[1])
     if addres not in client:
-        client.append(addres)# Если такова клиента нету , то добавить
+        client.append(addres) # Если такова клиента нету , то добавить
     for clients in client:
-        if clients == addres:
-            continue # Не отправлять данные клиенту который их прислал
-        sock.sendto(data, clients)
+        # if clients == addres:
+        #     continue # Не отправлять данные клиенту который их прислал
+        sock.sendto(pickle.dumps(level.levelheroes()), clients)
